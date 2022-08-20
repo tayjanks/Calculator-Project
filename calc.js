3bbda5913f8817de131e1073eb9725b65c836fce
tayjanks <taylorjenkins801@gmail.com>
2022-08-20 13:47:13 -0600

    back to subtraction

--- calc.js
+++ calc.js
@@ -13,23 +13,14 @@ reader.question("What would you like to calculate?", function(input){
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
-	if (mathSymbol=== "+"){
+	if (mathSymbol === "+"){
 		console.log(num1 + num2);
-	};
-	if (mathSymbol=== "-"){
-		console.log(num1 - num2);
-	};
-	if (mathSymbol=== "*"){
-		console.log(num1 * num2);
-	};
+  };
 
-	if (mathSymbol=== "/"){
-		console.log(num1 / num2);
-	};
-
-
-
-
+  if (mathSymbol === "-"){
+	console.log(num1-num2);
+  };
+	
 
 	console.log("mathSymbol", mathSymbol);
 	console.log("num1", num1);
