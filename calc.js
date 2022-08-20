9b53dc1dc5d9a404bb9b5bc1052e4edb4cde4d32
tayjanks <taylorjenkins801@gmail.com>
2022-08-20 14:21:28 -0600

    removing stupid semicolons to make it actually work

--- calc.js
+++ calc.js
@@ -29,8 +29,8 @@ reader.question("What would you like to calculate?", function(input){
 	console.log(num1/num2);
   };
 
-  if (mathSymbol === "sqrt"); {
-	console.log(Math.sqrt(num1));
+ if (mathSymbol === "sqrt") {
+console.log(Math.sqrt(num1));
   };
 	
 
