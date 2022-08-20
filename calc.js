1b854bcb46520fd34a5cab42c489078902e83d0e
tayjanks <taylorjenkins801@gmail.com>
2022-08-20 13:48:22 -0600

    multiplication added

--- calc.js
+++ calc.js
@@ -20,6 +20,10 @@ reader.question("What would you like to calculate?", function(input){
   if (mathSymbol === "-"){
 	console.log(num1-num2);
   };
+
+  if (mathSymbol === "*"){
+	console.log(num1*num2);
+  };
 	
 
 	console.log("mathSymbol", mathSymbol);
