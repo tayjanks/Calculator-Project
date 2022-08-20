9accf895472498fbbfa89030557b4d99008aefea
tayjanks <taylorjenkins801@gmail.com>
2022-08-20 13:25:08 -0600

    Added Addition

--- calc.js
+++ calc.js
@@ -13,7 +13,27 @@ reader.question("What would you like to calculate?", function(input){
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
+	if (mathSymbol=== "+"){
+		console.log(num1 + num2);
+	};
+	if (mathSymbol=== "-"){
+		console.log(num1 - num2);
+	};
+	if (mathSymbol=== "*"){
+		console.log(num1 * num2);
+	};
 
+	if (mathSymbol=== "/"){
+		console.log(num1 / num2);
+	};
+
+
+
+
+
+	console.log("mathSymbol", mathSymbol);
+	console.log("num1", num1);
+	console.log("num2", num2);
 
 	// This line closes the connection to the command line interface.
 	reader.close()
